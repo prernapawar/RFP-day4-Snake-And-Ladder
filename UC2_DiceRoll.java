50b00d9296a4a62cc71209a9f3c0444eb424978f
prernapawar <100364823+prernapawar@users.noreply.github.com>
2022-04-07 18:42:38 +0530

    added dice roll program

---
+++ UC2_DiceRoll.java
@@ -0,0 +1,15 @@
+package Com.Bridgelabz;
+/**
+ * @author Prerna
+ *
+ */
+public class UC2_DiceRoll {
+  //calling main function
+	public static void main(String[] args) {
+  //Take the int variables
+		int position=0;
+		int randomCheck=(int) Math.floor(Math.random() * 10) % 6+1;
+
+	}
+
+}
