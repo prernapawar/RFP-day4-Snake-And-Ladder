ea4914a8e4ffbd92a29f6689362978d0bcfb6246
prernapawar <100364823+prernapawar@users.noreply.github.com>
2022-04-07 18:41:11 +0530

    added program with single player

---
+++ UC1_InitialPosition.java
@@ -0,0 +1,12 @@
+package Com.Bridgelabz;
+/**
+ * @author Prerna
+ *
+ */
+public class UC1_InitialPosition {
+	//calling main function
+		public static void main(String[] args) {
+		int Position = 0;
+  }
+ 
+}
